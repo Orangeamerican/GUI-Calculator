d64d24706da0f09c3edae0db0c10b61b02e5eb62
Orangeamerican <94382093+Orangeamerican@users.noreply.github.com>
2021-11-15 09:18:56 -0600

    Add files via upload

---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+
+        private string hold;
+        private string value;
+        string operation = "";
+
+        private Solver solving = new Solver();
+        public Form1()
+        {
+            InitializeComponent();
+            hold = "" + Environment.NewLine + Environment.NewLine + "----------------------" + Environment.NewLine;
+            textBox1.Text = "" + Environment.NewLine + Environment.NewLine + "----------------------" + Environment.NewLine;
+        }
+        
+        //clears everything pressed
+        private void AC_Click(object sender, EventArgs e)
+        {
+            value = "";
+            textBox1.Text = value + hold;
+        }
+
+        //adds plus or minus
+        private void plusOrminus_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            value += button.Text;
+            textBox1.Text = value + hold;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            value += button.Text;
+            textBox1.Text = value + hold;
+        }
+
+        private void operation_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operation += button.Text;
+            if (button.Text == "x")
+            {
+                value += "*";
+                textBox1.Text = value + hold;
+            }
+            else
+            {
+                value += button.Text;
+                textBox1.Text = value + hold;
+            }
+        }
+
+        private void equals_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Calculator/Calculator/Solver.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Solver : ISolver
+    {
+        char[] oper = { '*', '/', '%', '-', '+', '.' };
+        string operations = "";
+        public void Accumulate(string s)
+        {
+
+        }
+        public void Clear()
+        {
+
+        }
+        public double Solve(string s)
+        {
+            double result = 0;
+            for (int i = 0; i < operations.Length; i++)
+            {
+
+            }
+
+            return Convert.ToDouble(result);
+        }
+    }
+
+    delegate void GreetingMethod(string s);
+
+    class Program
+    {
+        static void FirstGreeting(string s)
+        {
+            Console.WriteLine(s);
+        }
+        static void SecondGreeting(string s)
+        {
+            Console.WriteLine("I say again {0}!", s);
+        }
+        static void Main(String[] args)
+        {
+            GreetingMethod del = new GreetingMethod(FirstGreeting);
+            del += new GreetingMethod(SecondGreeting);
+
+            del("Hello");
+
+            //del("Hello");
+
+            //GreetingMethod del2 = new GreetingMethod(SecondGreeting);
+            //del2("Guten tag");
+
+            //FlexGreeting(del, "Hola");
+            //FlexGreeting(del2, "Bonjour");
+        }
+
+        static void FlexGreeting(GreetingMethod gd, string s)
+        {
+            gd(s);
+        }
+    }
+
+    public delegate void ChangedEventHandler(object sender, EventArgs e);
+
+    public class Student
+    {
+        int idNum;
+        double gpa;
+        public event ChangedEventHandler Changed;
+
+        public int IdNum
+        {
+            get { return idNum; }
+            set
+            {
+                idNum = value;
+                Changed(this, EventArgs.Empty);
+            }
+        }
+        public double Gpa
+        {
+            get { return gpa; }
+            set
+            {
+                gpa = value;
+                Changed(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public class EventListner
+    {
+        Student stu;
+        public EventListner(Student student)
+        {
+            stu = student;
+            stu.Changed += new ChangedEventHandler(StudentChanged);
+        }
+        void StudentChanged (object sender, EventArgs e)
+        {
+            Console.WriteLine("The student data has changed!");
+            Console.WriteLine("ID#{0} GPA:{1}", stu.IdNum, stu.gpa);
+        }
+    }
+}
